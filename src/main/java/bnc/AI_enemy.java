3e0e807f35ee6cff56deca079f66e3e7f5cf7289
Дмитрий Злобин <dmitriy.zlobin@eltex.loc>
2017-11-09 08:21:21 +0700

    Revert "fixed randmizator"
    This reverts commit 08e5ddc

--- src/main/java/bnc/AI_enemy.java
+++ src/main/java/bnc/AI_enemy.java
@@ -26,6 +26,7 @@ public class AI_enemy {
         int nextRand;
         while (sb.length() < 4) {
             nextRand = r.nextInt(10);
+            if (nextRand == 0 && sb.length() == 0) continue;
             if (sb.indexOf(String.valueOf(nextRand)) == -1) sb.append(nextRand);
         }
         return sb.toString();
